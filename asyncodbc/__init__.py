434a514396c8e2c9b7ee72b61dea0ec2eedb54ee
long2ice <long2ice@gmail.com>
2022-05-20 12:42:52 +0800

    add pypi.yaml

--- asyncodbc/__init__.py
+++ asyncodbc/__init__.py
@@ -5,7 +5,7 @@ from pyodbc import dataSources as _dataSources
 from .connection import Connection, connect
 from .pool import Pool, create_pool
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["connect", "Connection", "create_pool", "Pool", "data_sources"]
 
 
